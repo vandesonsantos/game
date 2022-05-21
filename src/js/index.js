a01975a87448eb1d8e670a73e81688bf82e2ba6e
vandesonsantos <92255361+vandesonsantos@users.noreply.github.com>
2022-05-21 10:59:20 -0300

    js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
-alert('Olá') 
+/* 
+OBJETIVO 1 - Quando passar o mouse em cima do personagem temos que: 
+
+    - Colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele
+
+    - Retirar a classe selecionado do personagem que está selecionado
+*/
+
+const homemDeFerro =
