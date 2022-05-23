578d8ba8e7b412a78996aada5ea6a4ba28602f60
vandesonsantos <92255361+vandesonsantos@users.noreply.github.com>
2022-05-23 11:17:01 -0300

    JavaScript

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,18 @@ OBJETIVO 1 - Quando passar o mouse em cima do personagem temos que:
     - Retirar a classe selecionado do personagem que está selecionado
 */
 
-const homemDeFerro =
+/* ONJETIVO 2 - Quando passar o mouse em cima do personagem, trocar a imagem e nome do personagem grande
+    - Alterar a imagem do jogador 1
+    - Alterar o nome do jogador 1
+*/
+
+const personagens = document.querySelectorAll('.personagem')
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        const personagemSelecionado = document.querySelector('.selecionado')
+        personagemSelecionado.classList.remove('selecionado')
+
+        personagem.classList.add('selecionado')
+    })
+})
