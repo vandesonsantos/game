c3d38f96b0f4e28ec24e6d457c9f692e00a63927
vandesonsantos <92255361+vandesonsantos@users.noreply.github.com>
2022-05-23 11:55:14 -0300

    js personagem

--- src/js/index.js
+++ src/js/index.js
@@ -4,20 +4,45 @@ OBJETIVO 1 - Quando passar o mouse em cima do personagem temos que:
     - Colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele
 
     - Retirar a classe selecionado do personagem que está selecionado
-*/
 
-/* ONJETIVO 2 - Quando passar o mouse em cima do personagem, trocar a imagem e nome do personagem grande
+OBJETIVO 2 - Quando passar o mouse em   cima do personagem, trocar a imagem e nome do personagem grande.
+
     - Alterar a imagem do jogador 1
+
     - Alterar o nome do jogador 1
 */
+
 
 const personagens = document.querySelectorAll('.personagem')
 
 personagens.forEach((personagem) => {
     personagem.addEventListener('mouseenter', () => {
+        /*
+            OBJETIVO 1 - Quando passar o mouse em cima do personagem temos que: 
+
+                - Colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele
+
+                - Retirar a classe selecionado do personagem que está selecionado
+        */
         const personagemSelecionado = document.querySelector('.selecionado')
         personagemSelecionado.classList.remove('selecionado')
 
         personagem.classList.add('selecionado')
-    })
-})
+
+        /*
+        OBJETIVO 2 - Quando passar o mouse em cima do personagem, trocar a imagem e nome do personagem grande
+
+            - Alterar a imagem do jogador 1
+
+            - Alterar o nome do jogador 1
+        
+        */
+
+        const idSelecionado = personagem.attributes.id.value;
+
+        const imagemJogador1 = document.getElementById('personagem-jogador-1');
+        imagemJogador1.src = `./src/imagens/${idSelecionado}.png`;
+    });
+});
+
+
