04773a737e8fbba1874cc7c196d40f120927b46e
Vandeson Santos <vandeson.eng.quimica@gmail.com>
2023-05-22 11:48:31 -0300

    Corriigindo Erro no JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,56 +1,49 @@
 /* 
-OBJETIVO 1 - Quando passar o mouse em cima do personagem temos que: 
+    OBJETIVO 1 - Quando passar o mouse em cima do personagem temos que: 
+        Passo 1 - Colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele
 
-    - Colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele
+        Passo 2 - Retirar a classe selecionado do personagem que está selecionado
 
-    - Retirar a classe selecionado do personagem que está selecionado
 
-OBJETIVO 2 - Quando passar o mouse em   cima do personagem, trocar a imagem e nome do personagem grande.
+    OBJETIVO 2 - Quando passar o mouse em cima do personagem da listagem, trocar a imagem e nome do personagem grande.
+        Passo 1 - Alterar a imagem do jogador 1
 
-    - Alterar a imagem do jogador 1
-
-    - Alterar o nome do jogador 1
+        Passo 2 - Alterar o nome do jogador 1
 */
 
 
-const personagens = document.querySelectorAll('.personagem')
+const personagens = document.querySelectorAll(".personagem")
 
 personagens.forEach((personagem) => {
-    personagem.addEventListener('mouseenter', () => {
-        
-        const idSelecionado = personagem.attributes.id.value;
-
-        if(idSelecionado === 'ultron') return;
+    personagem.addEventListener("mouseenter", () => {
         /*
             OBJETIVO 1 - Quando passar o mouse em cima do personagem temos que: 
+        Passo 1 - Colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele
 
-                - Colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele
-
-                - Retirar a classe selecionado do personagem que está selecionado
+        Passo 2 - Retirar a classe selecionado do personagem que está selecionado
         */
-        const personagemSelecionado = document.querySelector('.selecionado')
-        personagemSelecionado.classList.remove('selecionado')
+        const personagemSelecionado = document.querySelector(".selecionado")
 
-        personagem.classList.add('selecionado')
+        personagemSelecionado.classList.remove("selecionado")
+        personagem.classList.add("selecionado")
+
 
         /*
-        OBJETIVO 2 - Quando passar o mouse em cima do personagem, trocar a imagem e nome do personagem grande
+            OBJETIVO 2 - Quando passar o mouse em cima do personagem da listagem, trocar a imagem e nome do personagem grande.
+        Passo 1 - Alterar a imagem do jogador 1
 
-            - Alterar a imagem do jogador 1
-
-            - Alterar o nome do jogador 1
-        
+        Passo 2 - Alterar o nome do jogador 1
         */
+        const idSelecionado = personagem.attributes.id.value
+        if(idSelecionado === 'ultron') return;
 
-
-        const imagemJogador1 = document.getElementById('personagem-jogador-1');
-        imagemJogador1.src = `./src/imagens/${idSelecionado}.png`;
+        const imagemJogador1 = document.getElementById("personagem-jogador-1")
+        imagemJogador1.src = `./src/imagens/${idSelecionado}.png`
 
         const nomeJogador1 = document.getElementById('nome-jogador-1');
         const nomeSelecionado = personagem.getAttribute('data-name');
         nomeJogador1.innerHTML = nomeSelecionado;
-
-    });
-});
+    })
+})
 
 
